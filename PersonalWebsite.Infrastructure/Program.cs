a67a91dc16e6b92169af57ba317a91a0982dfdf1
Favian Flores <weblogs_serve_0e@icloud.com>
2024-10-02 19:07:14 -0400

    add cdk project

---
+++ PersonalWebsite.Infrastructure/Program.cs
@@ -0,0 +1,30 @@
+﻿using Amazon.CDK;
+using PersonalWebsite.Infrastructure;
+
+// ReSharper disable UnusedVariable
+
+App app = new();
+IPersonalWebsiteConfig config = new PersonalWebsiteProductionConfig();
+
+Environment dnsEnvironment = new()
+{
+    Account = config.AwsDnsAccountId,
+    Region = config.AwsDnsRegionId
+};
+
+DefaultStackSynthesizerProps dnsStackSynthesizerProps = new()
+{
+    Qualifier = config.DnsQualifierId
+};
+DefaultStackSynthesizer synthesizer = new(dnsStackSynthesizerProps);
+
+StackProps personalWebsiteDnsStackProps = new()
+{
+    Description = "Contains dns settings for personal website app.",
+    Env = dnsEnvironment,
+    Synthesizer = synthesizer
+};
+
+PersonalWebsiteDnsStack personalWebsiteDnsStack = new(app, config.StackName, personalWebsiteDnsStackProps, config);
+
+app.Synth();
